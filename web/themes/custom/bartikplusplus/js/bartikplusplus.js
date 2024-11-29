c1f6b5cfc5b3b2142c9c33047653f512e5f726ef
GeorgiadisChristos <189996083+GeorgiadisChristos@users.noreply.github.com>
2024-11-29 11:13:37 +0200

    Bartik subtheme, override node template

---
+++ web/themes/custom/bartikplusplus/js/bartikplusplus.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * BartikPlusPlus behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.bartikplusplus = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
